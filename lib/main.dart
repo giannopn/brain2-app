1b393c1b0b3ee64b13c040cccecc3784abdf5c2c
Nikos Giannopoulos <68999985+giannopn@users.noreply.github.com>
2025-12-05 14:11:25 +0200

    Initialize Flutter project structure

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Text(
+            'Hello Flutter!',
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
